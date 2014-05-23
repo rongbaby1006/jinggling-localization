9ed9101431c169f4ff7e463df4cddd2fe0ec5c6b
Mamong <chyy.meng@gmail.com>
2014-05-23 09:33:08 +0800

    Create en_Localizable.c

---
+++ en_Localizable.c
@@ -0,0 +1,212 @@
+/* 
+  Localizable.strings
+  jinggling-mobile
+
+  Created by Marco on 14-5-21.
+
+*/
+
+
+/**********************************************************
+                        Favorites
+ ***********************************************************/
+// title
+"Favorites" = "Favorites";
+
+// ABPeoplePickerNavigationController
+"Contacts" = "Contacts";
+
+// left barButton Item
+"Edit" = "Edit";
+"Done" = "Done";
+
+
+/**********************************************************
+                        Recents
+ ***********************************************************/
+// title
+"Recents" = "Recents";
+
+// left barButton Item
+//"Edit" = "Edit";
+"Clear" = "Clear";
+
+// right barButton Iem
+//"Done" = "Done";
+
+// action sheet
+"Cancel" = "Cancel";
+"Clear Recent Calls" = "Clear Recent Calls";
+
+// cell for recent call without known name
+"unknown" = "unknown";
+
+/**********************************************************
+                        Keypad
+ ***********************************************************/
+// tabBar Item title
+"Keypad" = "Keypad";
+
+
+/**********************************************************
+        Contacts（ABPeoplePickerNavigationController）
+ ***********************************************************/
+// ABPeoplePickerNavigationController
+//"Contacts" = "Contacts";
+
+
+/**********************************************************
+                        Settings
+***********************************************************/
+// title
+"Settings" = "Settings";
+/*========================== section 0 =========================*/
+
+/*############*/"Account:" = "Account:";
+//addition(dial number)
+"[Number not Set]" = "[Number not Set]";
+
+
+/*############*/"User's Guide" = "User's Guide";
+//addition(alert)
+"Failed" = "Failed";
+"Please try activate service first." = "Please try activate service first.";
+"OK" = "OK";
+/*--------SimpleWebViewController------*/
+// title
+// "User's Guide" = "User's Guide";
+/*---------------------------------*/
+
+
+
+/*############*/"Check Rates" = "Check Rates";
+/*-------RatesViewController------*/
+// navigation item title
+"Rates" = "Rates";
+/*--------------------------------*/
+
+/*############*/"Reactivate Service" = "Reactivate Service";
+
+
+/*############*/"Account Service" = "Account Service";
+// addition(HUD)
+"Contact to Server" = "Contact to Server";
+
+
+
+/*========================== section 1 =========================*/
+/*############*/"Tell Friends" = "Tell Friends";
+/*-------TellFriendsViewController------*/
+// navigation item title
+//"Tell Friends" = "Tell Friends";
+
+//SMS composerSheet,picker body
+"Jinggling is an awesome int'l call app - simpler, cheaper, clearer.\nEnter my phone number as the referrer for extra bonus. http://jinggling.com" = "Jinggling is an awesome int'l call app - simpler, cheaper, clearer.\nEnter my phone number as the referrer for extra bonus. http://jinggling.com";
+
+//email,subject
+"Jinggling International Call App" = "Jinggling International Call App";
+//email,body
+"Jinggling is an awesome int'l call app - simpler, cheaper, clearer.\nEnter my phone number as the referrer for extra bonus. http://jinggling.com" = "Jinggling is an awesome int'l call app - simpler, cheaper, clearer.\nEnter my phone number as the referrer for extra bonus. http://jinggling.com";
+
+//SMS Result,alert view
+"SMS Error" = "SMS Error";
+"Error in sending SMS" = "Error in sending SMS";
+"OK" = "OK";
+
+//email Result
+"Result: canceled" = "Result: canceled";
+"Result: saved" = "Result: saved";
+"Result: sent" = "Result: sent";
+"Result: failed" = "Result: failed";
+"Result: not sent" = "Result: not sent";
+
+/*--------------------------------*/
+
+/*############*/"Rate the App!" = "Rate the App!";
+
+/*========================== section 2 =========================*/
+
+/*############*/"Version" = "Version";
+
+
+
+/*############*/"Feedback" = "Feedback";
+/*-------FeedbackViewController----*/
+// title
+"Feedback" = "Feedback";
+
+// cell 1 placeholder
+"Email Address (Optional):" = "Email Address (Optional):";
+
+// HUD
+"Sent" = "Sent";
+
+// right barButton Item title
+"Send" = "Send";
+/*---------------------------------*/
+
+/*--------FeedbackCategoryViewController----*/
+// title
+"Topics" = "Topics";
+
+// datasource
+"Voice Call Quality" = "Voice Call Quality";
+"Service Activation" = "Service Activation";
+"Payment & Charge"   = "Payment & Charge";
+"Software & Device"  = "Software & Device";
+"General Feedback"   = "General Feedback";
+/*-----------------------------------------*/
+
+
+
+
+/*############*/"Language" = "Language";
+
+
+
+/**********************************************************
+                        Language
+ ***********************************************************/
+//title
+"Language" = "Language";
+
+// HUD
+"Setting language..." = "Setting language...";
+
+// left barbutton item
+"Cancel" = "Cancel";
+
+// right barbutton item
+"Save" = "Save";
+
+
+
+
+
+
+/**********************************************************
+                        Utils
+ ***********************************************************/
+//friendlyCallingTime:
+"Yesterday" = "Yesterday";
+
+
+/**********************************************************
+                ActivateServiceViewController
+ ***********************************************************/
+
+/**********************************************************
+                ActivateServiceStep1ViewController
+ ***********************************************************/
+
+
+/**********************************************************
+                ActivateServiceStep2ViewController
+ ***********************************************************/
+
+
+
+/**********************************************************
+                ActivateServiceStep3ViewController
+ ***********************************************************/
+
