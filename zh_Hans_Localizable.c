e7c3d200ea37cde4ce401cd3efc5b30a9bd8d826
Mamong <chyy.meng@gmail.com>
2014-05-23 09:34:45 +0800

    Create zh_Hans_Localizable.c

---
+++ zh_Hans_Localizable.c
@@ -0,0 +1,205 @@
+/* 
+  Localizable.strings
+  jinggling-mobile
+
+  Created by mamong on 14-5-21.
+
+*/
+
+/**********************************************************
+ Favorites
+ ***********************************************************/
+// title[not in use]
+//"Favorites" = "喜爱";
+
+//navigation Item title
+"Favorites" = "喜爱";
+
+// ABPeoplePickerNavigationController
+"Contacts" = "通讯录";
+
+// left barButton Item
+"Edit" = "编辑";
+"Done" = "完成";
+
+
+/**********************************************************
+ Recents
+ ***********************************************************/
+// title[not in use]
+//"Recents" = "最近";
+
+//navigation Item title
+"Recent Calls" = "最近通话";
+
+// left barButton Item
+//"Edit" = "Edit";
+"Clear" = "清除";
+
+// right barButton Iem
+//"Done" = "Done";
+
+// action sheet
+"Cancel" = "取消";
+"Clear Recent Calls" = "清除近期通话记录";
+
+// cell for recent call without known name
+"unknown" = "未知";
+
+/**********************************************************
+ Keypad
+ ***********************************************************/
+// tabBar Item title
+"Keypad" = "拨号盘";
+
+/**********************************************************
+ Contacts（ABPeoplePickerNavigationController）
+ ***********************************************************/
+// ABPeoplePickerNavigationController
+//"Contacts" = "Contacts";
+
+
+/**********************************************************
+                        Settings
+ ***********************************************************/
+// title
+"Settings" = "设置";
+/*========================== section 0 =========================*/
+
+/*############*/"Account:" = "账户:";
+//addition(dial number)
+"[Number not Set]" = "[号码未设置]";
+
+
+/*############*/"User's Guide" = "用户指南";
+//addition(alert)
+"Failed" = "失败";
+"Please try activate service first." = "请先激活服务.";
+"OK" = "确定";
+/*---------SimpleWebViewController---------*/
+// title
+// "User's Guide" = "用户指南";
+/*-----------------------------------------*/
+
+/*############*/"Check Rates" = "评级";
+/*-----------RatesViewController----------*/
+// navigation item title
+"Rates" = "评级";
+/*-----------------------------------------*/
+
+/*############*/"Reactivate Service" = "重新激活服务";
+
+/*############*/"Account Service" = "账户服务";
+// addition(HUD)
+"Contact to Server" = "连接至服务器";
+
+
+/*========================== section 1 =========================*/
+/*############*/"Tell Friends" = "告诉朋友";
+/*-------TellFriendsViewController------*/
+// navigation item title
+//"Tell Friends" = "Tell Friends";
+
+//SMS composerSheet,picker body
+//"Jinggling is an awesome int'l call app - simpler, cheaper, clearer.\nEnter my phone number as the referrer for extra bonus. http://jinggling.com" = "Jinggling is an awesome int'l call app - simpler, cheaper, clearer.\nEnter my phone number as the referrer for extra bonus. http://jinggling.com";
+
+//email,subject
+"Jinggling International Call App" = "Jinggling 国际呼叫应用";
+//email,body
+"Jinggling is an awesome int'l call app - simpler, cheaper, clearer.\nEnter my phone number as the referrer for extra bonus. http://jinggling.com" = "Jinggling 是一个非常棒的国际呼叫应用，更加简单、便宜、智能.\n输入我的号码，推荐人将获得额外的奖励. http://jinggling.com";
+
+//SMS Result,alert view
+"SMS Error" = "短信错误";
+"Error in sending SMS" = "发送短信时出错";
+"OK" = "确定";
+
+//email Result
+"Result: canceled" = "结果: 已取消";
+"Result: saved" = "结果: 已保存";
+"Result: sent" = "结果: 已发送";
+"Result: failed" = "结果: 失败";
+"Result: not sent" = "结果: 未发送";
+
+/*--------------------------------*/
+
+/*############*/"Rate the App!" = "为应用打分!";
+
+/*========================== section 2 =========================*/
+
+/*############*/"Version" = "版本";
+
+/*############*/"Feedback" = "反馈";
+/*-------FeedbackViewController----*/
+// title
+//"Feedback" = "反馈";
+
+// cell 1 placeholder
+"Email Address (Optional):" = "邮箱地址 (可选):";
+
+// HUD
+"Sent" = "已发送";
+
+// right barButton Item title
+"Send" = "发送";
+/*---------------------------------*/
+
+/*--------FeedbackCategoryViewController----*/
+// title
+"Topics" = "主题";
+
+// datasource
+"Voice Call Quality" = "通话质量";
+"Service Activation" = "服务激活";
+"Payment & Charge"   = "支付 & 缴费";
+"Software & Device"  = "软件 & 设备";
+"General Feedback"   = "普通反馈";
+/*-----------------------------------------*/
+
+
+"Language" = "语言";
+
+//*********************************************************
+
+
+/**********************************************************
+                        Language
+ ***********************************************************/
+//title
+"Language" = "语言";
+
+// HUD
+"Setting language..." = "正在设置语言...";
+
+// left barbutton item
+"Cancel" = "取消";
+
+// right barbutton item
+"Save" = "保存";
+//*********************************************************
+
+
+/**********************************************************
+                            Utils
+ ***********************************************************/
+//friendlyCallingTime:
+"Yesterday" = "昨天";
+
+
+/**********************************************************
+ ActivateServiceViewController
+ ***********************************************************/
+
+/**********************************************************
+ ActivateServiceStep1ViewController
+ ***********************************************************/
+
+
+/**********************************************************
+ ActivateServiceStep2ViewController
+ ***********************************************************/
+
+
+
+/**********************************************************
+ ActivateServiceStep3ViewController
+ ***********************************************************/
